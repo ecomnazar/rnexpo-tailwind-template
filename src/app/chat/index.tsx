793d14a027ca6a25c5ee7cbaa19c268602e45e0c
ecomnazar <ecomnazar.me@gmail.com>
2024-11-20 16:08:54 +0500

    [added]: tailwind

---
+++ src/app/chat/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-blue-500 text-2xl">Hello world!</Text>
+    </View>
+  );
+}
